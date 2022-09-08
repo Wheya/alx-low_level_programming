512710db1ed97480dfc4521346b42def2c93291a
Wheya <rejoicesimon7@gmail.com>
2022-09-08 04:57:26 -0700

    task 4 of hello world project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,22 @@
 /**
  * main - entry point
  *
- * Return: 0 after printing the function
+ * Return: always 0 (Success)
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle"):
-return (0):
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	print(size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	print("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	print("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	print("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
+ls
+rm ls
