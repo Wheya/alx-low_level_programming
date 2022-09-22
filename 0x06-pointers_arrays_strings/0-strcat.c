df32d78ca872c8e6a46914ee0df19767978a06e4
Wheya <rejoicesimon7@gmail.com>
2022-09-22 03:21:07 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int length = 0;
+int length = 0;
 
-	int i;
+int i;
 
-	while (dest[length] != '\0')
-	{
-		length++;
-	}
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[length] = src[i];
-		i++;
-		length++;
-	}
-	dest[length] = '\0';
-	return (dest);
+while (dest[length] != '\0')
+{
+length++;
+}
+i = 0;
+while (src[i] != '\0')
+{
+dest[length] = src[i];
+i++;
+length++;
+}
+dest[length] = '\0';
+return (dest);
 }
