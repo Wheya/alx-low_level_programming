b99b33d9c0a3afd943f808b763eb15184b6a8ad8
Wheya <rejoicesimon7@gmail.com>
2022-09-28 05:10:30 -0700

    Recursion task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,50 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings
- * @s1:first string
- * @s2: second string
- * Return: 1 if are identical, 0 if not
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
  */
-int wildcmp(char *s1, char *s2)
+
+int _strlen_recursion(char *s)
 {
-if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-return (0);
-if (*s1 == '\0' && *s2 == '\0')
-return (1);
-if (*s1 == *s2)
-return (wildcmp(s1 + 1, s2 + 1));
-if (*s2 == '*')
-return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-return (0);
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
 }
