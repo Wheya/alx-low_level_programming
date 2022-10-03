b4011ffb735d05caba237b00022cd10fb5f9f8ce
Wheya <rejoicesimon7@gmail.com>
2022-10-03 08:38:43 -0700

    malloc, free task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,32 +8,34 @@
 
 char *create_array(unsigned int size, char c)
 {
-char *buffer;
-unsigned int position;
+	char *buffer;
+	unsigned int position;
 
-if (size == 0)
-{
-return (NULL);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	/*Define values with malloc*/
+	buffer = (char *) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+		{
+			return (NULL);
+		}
+
+	else 
+	{
+		position = 0;
+		while (position < size) /*While for array*/  
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+		
+		return (buffer);
+	}
+
 }
 
-/*Define values with malloc*/
-buffer = (char *) malloc(size * sizeof(c));
 
-if (buffer == 0)
-{
-return (NULL);   
-}
-
-else
-{
-position = 0;
-while (position < size) /*While for array*/
-{
-*(buffer + position) = c;
-position++;
-}
-
-return (buffer);
-}
-
-}
